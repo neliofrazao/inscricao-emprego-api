c9f82a903203173446df532426bd5b84508a8214
Lab05usuario19 <lab05usuario19@ACADEMICOS.COM>
2018-06-02 13:58:45 -0300

    [UPDATE] Ajustando o mapeamento da entidade candidato

--- src/Domain/Model/ExperienciaProfissional.php
+++ src/Domain/Model/ExperienciaProfissional.php
@@ -1,13 +1,6 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: lab05usuario19
- * Date: 02/06/2018
- * Time: 11:42
- */
 
 namespace Domain\Model;
-
 
 class ExperienciaProfissional
 {
@@ -15,25 +8,29 @@ class ExperienciaProfissional
      * @var int
      */
     private $idExperienciaProfissional;
+
     /**
      * @var string
      */
     private $cargo;
+
     /**
      * @var string
      */
     private $descricao;
+
     /**
      * @var \DateTime
      */
     private $periodoInicial;
+
     /**
      * @var \DateTime
      */
     private $periodoFinal;
+
     /**
      * @var boolean
      */
     private $trabalhoAtual;
-
 }
--- src/Domain/Model/HabilidadeTecnica.php
+++ src/Domain/Model/HabilidadeTecnica.php
@@ -8,16 +8,15 @@
 
 namespace Domain\Model;
 
-
 class HabilidadeTecnica
 {
     /**
      * @var int
      */
     private $idHabilidadeTecnica;
+
     /**
      * @var string
      */
     private $descricao;
-
 }
