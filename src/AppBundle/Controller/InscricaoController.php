65e9468e69c32365811d6cf98bc1f06f8dd45d87
Lab05usuario19 <lab05usuario19@ACADEMICOS.COM>
2018-06-02 15:54:07 -0300

    [UPDATE] Criando a Action de Inscricao

--- src/AppBundle/Controller/InscricaoController.php
+++ src/AppBundle/Controller/InscricaoController.php
@@ -9,8 +9,10 @@
 namespace AppBundle\Controller;
 
 
+use Domain\Model\Inscricao;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class InscricaoController extends Controller
@@ -20,6 +22,13 @@ class InscricaoController extends Controller
      * @param Request $request
      */
     public function inscreverAction(Request $request ){
+        $serializerService = $this->get('infra.serializer.service');
 
+        try{
+            $inscricao = $serializerService->converte($request->getContent(), Inscricao::class);
+            dump($inscricao); die;
+        }catch (\Exception $exception){
+            return new  Response($exception->getMessage(), 404);
+        }
     }
 }
